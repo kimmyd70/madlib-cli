3e94fc4ba2f8644cce8114d8567dae9530d2898f
KIMBERLY DAMALAS <kimatcamp@yahoo.com>
2020-12-13 11:14:48 -0800

    files set up

---
+++ madlib_cli/madlib_cli.py
@@ -0,0 +1,22 @@
+# Print welcome with instructions
+
+
+# Create and test a read_template function that takes in a path to text file
+# and returns a stripped string of the file’s contents. should raise a 
+# FileNotFoundError if path is invalid.
+
+# Create and test a parse_template function that takes in a template string 
+# and returns a string with language parts removed and a separate list of those 
+# language parts.
+
+# Prompt user for inputs
+
+
+# Create and test a merge function that takes in a “bare” template and a list 
+# of user entered language parts, and returns a string with the language parts 
+# inserted into the template.
+
+
+# Print the completed MadLib to the command line
+
+# Write the completed MadLib to a new file
